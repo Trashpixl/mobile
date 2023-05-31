b741644901b01ed86708d46749584d47c6323f51
Thierry Lachance <101730754+Thierry-Lachance@users.noreply.github.com>
2023-05-31 20:01:50 +0000

    save

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 
 if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
     // redirects if mobile
-    window.location = "https://google.com"
+    location.replace("http://google.com");
    } 
