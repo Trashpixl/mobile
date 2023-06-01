40a3f06c9947b7b86a60572bbb86f715545f8899
Thierry Lachance <101730754+Thierry-Lachance@users.noreply.github.com>
2023-06-01 12:37:28 +0000

    save

--- scripts.js
+++ scripts.js
@@ -1,5 +1,8 @@
 
 if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
     // redirects if mobile
-    location.replace("http://google.com");
+    
    } 
+   else{
+    location.replace("http://trashpixl.github.io");
+   }
