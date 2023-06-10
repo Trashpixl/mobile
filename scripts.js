582bca9db2e1ef50c2c1a46b5156ced0a6700fd1
Thierry Lachance <lachancethierry6@gmail.com>
2023-06-10 12:37:27 -0400

    change the text

--- scripts.js
+++ scripts.js
@@ -1,8 +1,16 @@
-
 if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
     // redirects if mobile
     
-   } 
-   else{
+} 
+else{
     //location.replace("http://trashpixl.github.io");
-   }
+}
+function button1(){
+
+}
+function button2(){
+
+}
+function button3(){
+    
+}
